3a9987ffe1cdab1121c67ee19f50aeeb1aa55c9d
graslowsnail <graslowsnail@gmail.com>
2024-12-16 00:06:26 -0600

    set up db

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from app.routes.user_routes import user_routes
 from app.routes.main_routes import main_routes
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-
-    # Register Blueprints (routes)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
     app.register_blueprint(main_routes)
-
+    app.register_blueprint(user_routes)
     return app
 
