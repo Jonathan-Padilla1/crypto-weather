5ede542bd3ca35c51ea50c867013c8cf65670ddc
graslowsnail <graslowsnail@gmail.com>
2024-12-16 03:25:25 -0600

    changed db to postgres on Neon, added env package. updated user model

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,24 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
 from app.routes.user_routes import user_routes
 from app.routes.main_routes import main_routes
+
+# load env variables from .env
+load_dotenv()
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')  # Get from .env
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     db.init_app(app)
+
     app.register_blueprint(main_routes)
     app.register_blueprint(user_routes)
+
     return app
 
